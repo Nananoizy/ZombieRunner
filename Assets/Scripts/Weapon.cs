0fd795d3def120523170344215305fd6fc40deb7
nananoizy <morihuelacazorla@gmail.com>
2020-03-19 17:37:41 +0100

    Raycast from carabine

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range);
+        Debug.Log(hit.transform.name);
+    }
+}
